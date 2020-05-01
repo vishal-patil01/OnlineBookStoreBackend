f50bb58764fcfba3db3191f3638bf94b91161ed6
vishal-patil01 <vishalpatil01@hotmail.com>
2020-05-01 19:56:08 +0530

    Initial Commit : Project Created

---
+++ src/main/java/com/enigma/bookstore/OnlineBookStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookStoreBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBookStoreBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enigma/bookstore/OnlineBookStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookStoreBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
