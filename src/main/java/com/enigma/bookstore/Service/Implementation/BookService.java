8f7f70ff366629fb5b9d29f75855f7ba513f44a1
vishal-patil01 <vishalpatil01@hotmail.com>
2020-05-12 23:37:43 +0530

    Added : Generic Single api For Display,Search  & Sort books.

--- src/main/java/com/enigma/bookstore/Service/Implementation/BookService.java
+++ src/main/java/com/enigma/bookstore/Service/Implementation/BookService.java
@@ -1,11 +1,13 @@
 package com.enigma.bookstore.service.implementation;
 
 import com.enigma.bookstore.enums.FilterAttributes;
+import com.enigma.bookstore.exception.BookException;
 import com.enigma.bookstore.model.Book;
 import com.enigma.bookstore.repository.IBookRepository;
 import com.enigma.bookstore.service.IBookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +19,10 @@ public class BookService implements IBookService {
 
     @Override
     public Page<Book> fetchBooks(String searchText, int pageNo, FilterAttributes filterAttributes) {
-        return null;
+        PageRequest pageRequest = PageRequest.of(pageNo-1, 12, filterAttributes.sort);
+        Page<Book> books = bookRepository.fetchBooks(pageRequest, searchText);
+        if (books.hasContent())
+            return books;
+        throw new BookException("There Are No Books Available");
     }
 }
--- src/main/java/com/enigma/bookstore/enums/FilterAttributes.java
+++ src/main/java/com/enigma/bookstore/enums/FilterAttributes.java
@@ -1,4 +1,15 @@
 package com.enigma.bookstore.enums;
 
+import org.springframework.data.domain.Sort;
+
 public enum FilterAttributes {
+    DEFAULT(Sort.by("book_name")),
+    LOW_TO_HIGH(Sort.by("book_price")),
+    HIGH_TO_LOW(Sort.by("book_price").descending()),
+    NEW_ARRIVALS(Sort.by("publishing_year").descending());
+    public Sort sort;
+
+    FilterAttributes(Sort sort) {
+        this.sort = sort;
+    }
 }
