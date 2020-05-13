cc4abcabeb0bf6ac570ce0c46781cae5c45d4a3a
mlatekar <mlatekar@gmail.com>
2020-05-13 22:47:47 +0530

    Added : File Upload Upload Api Using MultipartFile

--- src/main/java/com/enigma/bookstore/Service/Implementation/AdminService.java
+++ src/main/java/com/enigma/bookstore/Service/Implementation/AdminService.java
@@ -3,17 +3,29 @@ package com.enigma.bookstore.service.implementation;
 import com.enigma.bookstore.dto.BookDTO;
 import com.enigma.bookstore.exception.BookException;
 import com.enigma.bookstore.model.Book;
+import com.enigma.bookstore.properties.ApplicationProperties;
 import com.enigma.bookstore.repository.IBookRepository;
 import com.enigma.bookstore.service.IAdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Service
 public class AdminService implements IAdminService {
 
     @Autowired
     private IBookRepository bookRepository;
+
+    @Autowired
+    private ApplicationProperties applicationProperties;
 
     @Override
     public String addBook(BookDTO bookDTO) {
@@ -31,6 +43,20 @@ public class AdminService implements IAdminService {
 
     @Override
     public String uploadImage(MultipartFile file) {
-        return null;
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        String fileBasePath = System.getProperty("user.dir") + applicationProperties.getUploadDir();
+        if (!(fileName.endsWith(".jpg") || fileName.endsWith(".jpeg") || fileName.endsWith(".png"))) {
+            throw new BookException("Only Image Files Can Be Uploaded");
+        }
+        Path path = Paths.get(fileBasePath + fileName);
+        try {
+            Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("bookstore/image/")
+                .path(fileName)
+                .toUriString();
     }
 }
